e5d32af624ce7924f1812ea4081b090078fff5c4
chamikaEkanayaka <chamikaekanayaka@icloud.com>
2023-02-08 12:54:26 +0530

    Master Edit 2023.02.08

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import NavBar from "./Components/NavBar";
 import TitleBody from "./Components/TitleBody";
+import BodyRibbon1 from "./Components/BodyRibbon1";
+import EventCreation from "./Pages/EventCreation";
 
 function App() {
   return (
     <div>
       <NavBar/>
       <TitleBody/>
+      <BodyRibbon1/>
+      <EventCreation/>
     </div>
   );
 }
