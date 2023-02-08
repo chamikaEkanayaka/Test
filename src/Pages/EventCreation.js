49ad1bb686b9d59749219de4fc2181f56e4e5d81
chamikaEkanayaka <chamikaekanayaka@icloud.com>
2023-02-08 12:51:06 +0530

    Main Updated 2023.02.08

---
+++ src/Pages/EventCreation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./EventCreationStyles.css";
+function EventCreation(){
+    return(
+        <>
+            <h1>hello</h1>
+        </>
+    )
+}
+export default EventCreation;
