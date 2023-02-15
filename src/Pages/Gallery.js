6e2785660ee83a034488659c2477e25dbc78792c
JayanithyaMadhushani <121443632+Jayanithyaa@users.noreply.github.com>
2023-02-15 12:09:15 +0530

    2023.02.15

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -12,13 +12,23 @@ function Gallery(){
             <p class="ScndP">Event Management</p>
             <div class="Rec_01"></div>
             <p class="RecText_01">All</p>
+            <div class="Rec_02"></div>
+            <p class="RecText_02">All</p>
+            <div class="Rec_03"></div>
+            <p class="RecText_03">All</p>
+            <div class="Rec_04"></div>
+            <p class="RecText_04">All</p>
+            <div class="Rec_05"></div>
+            <p class="RecText_05">All</p>
+            <div class="Rec_06"></div>
+            <p class="RecText_06">All</p>
             <div class="Midlimg"></div>
             <hr class="MdlHr"></hr>
             <img src={require("../Components/Filter.png")} alt="FilterImg" class="Fltrimg"></img>
             <p class="fltr">Filers</p>
             <div class="FltrBox"></div>
             <div class="FtrBg"></div>
-            <img src={require("../Components/FtrImg.png")} alt="FooterImg" class="FtrImg"></img>            
+                       
         </>
     );
 }
