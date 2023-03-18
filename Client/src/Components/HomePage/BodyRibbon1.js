be2b3632d3170f72707e7b7bac949b579d133978
JayanithyaMadhushani <121443632+Jayanithyaa@users.noreply.github.com>
2023-03-18 23:09:18 +0530

    Revert "Merge branch 'master' into Jayanithya"
    This reverts commit b3fb7856c099c8acb10159d415867e92035cd912, reversing
    changes made to 852c3ea01c893542a3b43f6396e820a2e0baa1e5.

--- Client/src/Components/HomePage/BodyRibbon1.js
+++ Client/src/Components/HomePage/BodyRibbon1.js
@@ -8,14 +8,7 @@ function BodyRibbon1(){
             <p class="br1t1">Our event management system makes event planning easier for all of our in-person, hybrid, and virtual events.</p>
             <div class="br1b1"></div>
             <p class="br1bt1">Get Started</p>
-            <img src={require("../Components/rightarrow.png")} alt="navarrowright" class="br1bt1nr"></img>
-            <img src={require("../Components/pcimage.png")} alt="pcimage" class="br1i1"></img>
-            <div class="br1el"></div>
-            <div class="evAwr">
-                <div class="evAwrb1"></div>
-                <div class="evAwrb2"><p class="evAwt">Events for employees<br/> Boost morale, reduce<br/> stress, and promote<br/> professional growth</p></div>
-                <div class="evAwri"></div>
-            </div>
+            <img src="navarrowright.png" alt="navarrowright" class="br1bt1nr"></img>
         </>
     )
 }
