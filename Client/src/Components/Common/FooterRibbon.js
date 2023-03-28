716f2aabff3e71bc0be8a3c6e6d95a7d5388c089
JayanithyaMadhushani <121443632+Jayanithyaa@users.noreply.github.com>
2023-03-28 10:54:48 +0530

    2023.03.28

---
+++ Client/src/Components/Common/FooterRibbon.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Common/FooterRibbon.css";
+
+function FooterRibbon(){
+    return(
+        <>
+            <div class="frb">
+                <div class="frbi">
+                    <p class="frbih">EveM</p>
+                    <p class="frbid">This event management software helps<br/> us keep track of all of our event's<br/> moving parts to help our event run<br/> more smoothly</p>
+                </div>
+                <div class="frbi">
+                    <p class="frbih">Keep In Touch</p>
+                    <p class="frbid">Subscribe to receive new updates,<br/> be the know about our events, and<br/> more.</p>
+                </div>
+                <div class="frbi">
+                    <img src={require("../Components/ifsevemlogo.png")} alt="ifsevemlogo"></img>
+                </div>
+            </div>
+            <div class="frb2">
+                <img src={require("../Components/socialmediaiconsimg.png")} alt="socialmediaiconsimg" class="frb2smii"></img>
+                <p class="frb2c">© 2023, EveM. Powered by Matrix</p>
+                <img src={require("../Components/paymentmethodsiconsimg.png")} alt="paymentmethodsiconsimg" class="frb2pmii"></img>
+            </div>
+        </>
+    );
+}
+export default FooterRibbon;
--- Client/src/Pages/HomePage.js
+++ Client/src/Pages/HomePage.js
@@ -1,11 +1,19 @@
+import NavBar from "../Components/Common/NavBar";
 import TitleBody from "../Components/HomePage/TitleBody";
 import BodyRibbon1 from "../Components/HomePage/BodyRibbon1";
+import "../Components/HomePage/HomePageStyles.css";
+import FooterRibbon from "../Components/Common/FooterRibbon";
+
 
 function HomePage() {
   return (
     <div>
+      <NavBar/>
       <TitleBody/>
       <BodyRibbon1/>
+      <div class="br2r">increasing exposure.<br/>seamless planning.<br/>amazing event.</div>
+      <div class="br3"></div>
+      <FooterRibbon/>
     </div>
   );
 }
